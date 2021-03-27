9507d5a358a17aafc1537619b8fad4b205413064
Vladimir-create <79356244+Vladimir-create@users.noreply.github.com>
2021-03-27 12:44:46 +0200

    Add files via upload

--- httpserv.go
+++ httpserv.go
@@ -3,67 +3,37 @@ package main
 import (
 	"net/http"
 	"io"
+	//"strings"
 	"io/ioutil"
 	"fmt"
 )
 
-type (
-	Action struct {
-		Action string `json:"action"`
-		ObjName string `json:"object"`
-	}
-	Teacher struct {
-		ID string  `json:"id"`
-		Salary float64 `json:"salary"`
-		Subject string `json:"subject"`
-		Classroom []string `json:"classroom"`
-		Person struct {
-			Name string `json:"name"`
-			Surname string `json:"surname"`
-			PersonalCode string `json:"personalCode"`
-		} `json:"person"`
-	}
-	Teachermutex struct {
-		Ch chan int
-	}
-	UpdateTeacher struct {
-		T Teacher `json:"data"`
-	}
-	CreateTeacher struct {
-		T Teacher `json:"data"`
-	}
-	DeleteTeacher struct {
-		Data struct {
-			ID string `json:"id"`
-		} `json:"data"`
-	}
-	ReadTeacher struct {
-		T Teacher
-		Data struct {
-			ID string `json:"id"`
-		} `json:"data"`
-	}
-)
 
 func Handler(w http.ResponseWriter, req *http.Request) {
 	if req.Method == "GET" {
-		w.Write()
+		res, err := http.Get("http://localhost:8080/")
+		if err != nil {
+			fmt.Println(err)
+		}
+		data, _ := ioutil.ReadAll( res.Body )
+		res.Body.Close()
+		err = ioutil.WriteFile("p.html", data, 0666)
+		if err != nil {panic(err) }	
+		res.Body.Close()
 	} 
 	if req.Method == "POST" {
-		data, err := io.ReadAll(req.Body)
-		req.Body.Close()
-		if err != nil {return }
-		
-		fmt.Printf("%s\n", data)
 		io.WriteString(w, "successful post")
-	} else {
-		w.WriteHeader(405)
 	}
-	
+	if req.Method == "PUT" {
+		io.WriteString(w, "successful put")
+	}
+	if req.Method == "DELETE" {
+		io.WriteString(w, "successful delete")
+	}
 }
 
 func Handler2(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "You are on page1")
+	io.WriteString(w, "You are on page1\n")
 }
 
 func main() {
